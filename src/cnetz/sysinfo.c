5182f706719d38e1d197aa7afece6235c1bfbc3d
Andreas Eversberg <jolly@eversberg.eu>
2016-08-07 17:17:38 +0200

    C-Netz: Documentation of some system informations

--- src/cnetz/sysinfo.c
+++ src/cnetz/sysinfo.c
@@ -20,17 +20,21 @@ void init_sysinfo(void)
 	si[0].fuz_rest = 38;
 	si[1].fuz_rest = 39;
 
-	si[0].mittel_umschalten = 5;
+	/* a low value causes quicker measurement results */
+	si[0].mittel_umschalten = 5; /* 0..5 */
 	si[1].mittel_umschalten = 5;
 
-	si[0].grenz_umschalten = 0;
-	si[1].grenz_umschalten = 0;
+	/* a low value is tollerant to bad quality */
+	si[0].grenz_umschalten = 0; /* 0..13 */
+	si[1].grenz_umschalten = 0; /* 0..13 */
 
-	si[0].mittel_ausloesen = 5;
+	/* a low value causes quicker measurement results */
+	si[0].mittel_ausloesen = 5; /* 0..5 */
 	si[1].mittel_ausloesen = 5;
 
-	si[0].grenz_ausloesen = 0;
-	si[1].grenz_ausloesen = 0;
+	/* a low value is tollerant to bad quality */
+	si[0].grenz_ausloesen = 0; /* 0..13 */
+	si[1].grenz_ausloesen = 0; /* 0..13 */
 
 	si[0].sperre = 0;
 	si[1].sperre = 0;
@@ -41,8 +45,9 @@ void init_sysinfo(void)
 	si[0].entfernung = 3;
 	si[1].entfernung = 3;
 
-	si[0].grenz_einbuchen = 1; /* worst case */
-	si[1].grenz_einbuchen = 1; /* worst case */
+	/* a low value is tollerant to bad quality */
+	si[0].grenz_einbuchen = 1; /* 1..7 */
+	si[1].grenz_einbuchen = 1;
 
 	si[0].fufst_prio = 1; /* normal pio */
 	si[1].fufst_prio = 1; /* normal pio */
