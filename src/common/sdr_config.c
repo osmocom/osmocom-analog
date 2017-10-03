ae29f84ed7a3d6e0f5e44fdbab0b938a22f3fc3c
Andreas Eversberg <jolly@eversberg.eu>
2017-10-03 10:23:04 +0200

    Fixed typo

--- src/common/sdr_config.c
+++ src/common/sdr_config.c
@@ -93,7 +93,7 @@ void sdr_config_print_help(void)
 
 void sdr_config_print_hotkeys(void)
 {
-	printf("Press 'i' key to toggle display of RX I/Q vector.\n");
+	printf("Press 'q' key to toggle display of RX I/Q vector.\n");
 	printf("Press 's' key to toggle display of RX spectrum.\n");
 }
 
